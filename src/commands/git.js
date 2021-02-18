32ec397781c79e4057fd95ae7692c1ab9bf11f91
Ellie-exe <ryiscool247@gmail.com>
2021-02-18 14:08:48 -0500

    Added more git commands

--- src/commands/git.js
+++ src/commands/git.js
@@ -11,6 +11,30 @@ module.exports.execute = async (message, args, utils) => {
         const option = args.shift().toLowerCase();
 
         switch (option) {
+            case 'status': {
+                exec('git status', (err, stdout, stderr) => {
+                    if (err) {
+                        const embed = new MessageEmbed()
+                            .setTitle('Error')
+                            .setDescription(`\`\`\`\n${err}\n\`\`\``)
+                            .setColor(process.env.color);
+
+                        message.channel.send(embed).catch(err => utils.logger.error(err));
+                        utils.logger.error(err);
+
+                    } else {
+                        const embed = new MessageEmbed()
+                            .setTitle('Git Status')
+                            .setDescription(`\`\`\`\n${stdout}\n${stderr}\n\`\`\``)
+                            .setColor(process.env.color);
+
+                        message.channel.send(embed).catch(err => {utils.logger.error(err)});
+                    }
+                });
+
+                break;
+            }
+
             case 'stage': {
                 if (args[0] === 'all') args.splice(0, 1, '.');
 
@@ -38,6 +62,45 @@ module.exports.execute = async (message, args, utils) => {
                             } else {
                                 const embed = new MessageEmbed()
                                     .setTitle('Changes Staged')
+                                    .setDescription(`\`\`\`\n${stdout}\n${stderr}\n\`\`\``)
+                                    .setColor(process.env.color);
+
+                                message.channel.send(embed).catch(err => {utils.logger.error(err)});
+                            }
+                        });
+                    }
+                });
+
+                break;
+            }
+
+            case 'unstage': {
+                if (args[0] === 'all') args.splice(0, 1, '.');
+
+                exec(`git restore ${args.join(' ')}`, (err, stdout, stderr) => {
+                    if (err) {
+                        const embed = new MessageEmbed()
+                            .setTitle('Error')
+                            .setDescription(`\`\`\`\n${err}\n\`\`\``)
+                            .setColor(process.env.color);
+
+                        message.channel.send(embed).catch(err => utils.logger.error(err));
+                        utils.logger.error(err);
+
+                    } else {
+                        exec('git status', (err, stdout, stderr) => {
+                            if (err) {
+                                const embed = new MessageEmbed()
+                                    .setTitle('Error')
+                                    .setDescription(`\`\`\`\n${err}\n\`\`\``)
+                                    .setColor(process.env.color);
+
+                                message.channel.send(embed).catch(err => utils.logger.error(err));
+                                utils.logger.error(err);
+
+                            } else {
+                                const embed = new MessageEmbed()
+                                    .setTitle('Changes Unstaged')
                                     .setDescription(`\`\`\`\n${stdout}\n${stderr}\n\`\`\``)
                                     .setColor(process.env.color);
 
@@ -120,10 +183,6 @@ module.exports.execute = async (message, args, utils) => {
                 });
 
                 break;
-            }
-
-            default: {
-                throw new Error('Command not found');
             }
         }
 
