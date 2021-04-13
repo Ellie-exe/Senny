4233e47c4fafde959b0882e4f9a5c8996d61d1d8
Ellie-exe <ryiscool247@gmail.com>
2021-04-13 18:21:16 -0400

    Added RedNet join message

--- src/events/guildMemberUpdate.js
+++ src/events/guildMemberUpdate.js
@@ -8,7 +8,17 @@ module.exports = async (oldMember, newMember, utils) => {
         if (newMember.guild.id !== '573272766149558272') return;
 
         if (oldMember.pending && !newMember.pending && !newMember.user.bot) {
+            const channel = newMember.guild.channels.cache.get('573272766611193867');
             await newMember.roles.add('578211559390576640');
+
+            await channel.send(
+                `Welcome to **RedNet**, ${newMember}! Be sure to read the rules in <#578226946786459649>, `+
+                `grab yourself some roles in <#781959946635444255>, and introduce yourself in <#647970191090909185>! `+
+                `If you have any questions, feel free to ask! We hope you have a great time here!\n`+
+                `https://ellie.hep.gg/welcome.gif`,
+                
+                {allowedMentions: {parse: []}}
+            );
 
             newMember.user.flags?.toArray().forEach(async flag => {
                 switch (flag) {
