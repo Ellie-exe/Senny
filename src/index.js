203f93fc3778dcd951d74ebd463518069d00c0eb
Ellie-exe <ryiscool247@gmail.com>
2021-10-29 18:11:31 -0400

    Simplified handlers and imports

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
-const {SlashCommandBuilder} = require('@discordjs/builders');
-const {Sequelize, DataTypes} = require('sequelize');
 const logger = require('@jakeyprime/logger');
 const discord = require('discord.js');
 
 const client = new discord.Client({intents: [discord.Intents.FLAGS.GUILDS]});
 
-const sequelize = new Sequelize('test', 'senny', 'pants777', {
-    host: 'localhost',
-    dialect: 'mariadb',
-    logging: msg => logger.info(msg),
-});
-
-global.SlashCommand = SlashCommandBuilder;
-global.MessageEmbed = discord.MessageEmbed;
-global.sequelize = sequelize;
-global.DataTypes = DataTypes;
-global.logger = logger;
+global.discord = discord;
 global.client = client;
+global.logger = logger;
 
 client.commands = new discord.Collection();
 
 const commands = require('./commands');
 Object.values(commands).forEach(command => {
-    client.commands.set(command.data.name, command);
+    client.commands.set(command.data[0].name, command);
 });
 
 const events = require('./events');
