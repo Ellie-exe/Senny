3cd63c740f7e3a722d3c10c45aec65a974f1bebd
Ellie-exe <ryiscool247@gmail.com>
2021-10-30 12:03:28 -0400

    Added a choose command

---
+++ src/commands/choose.js
@@ -0,0 +1,31 @@
+module.exports = {
+    /** @param {import('discord.js/typings').CommandInteraction} command */
+    async execute(command) {
+        try {
+            const list = command.options.getString('choices');
+            const choices = list.split(list.includes(',') ? /\s*,\s*/ : / +/);
+            const choice = choices[Math.floor(Math.random() * choices.length)];
+
+            await command.reply(`**List:** ${choices.join(', ')}\n**Choice:** ${choice}`);
+
+        } catch (err) {
+            logger.error(err);
+        }
+    },
+
+    data: [
+        {
+            type: 'CHAT_INPUT',
+            name: 'choose',
+            description: 'Have something randomly chosen from a list',
+            options: [
+                {
+                    type: 'STRING',
+                    name: 'choices',
+                    description: 'The list to choose from (either comma or space separated)',
+                    required: true
+                }
+            ]
+        }
+    ]
+};
