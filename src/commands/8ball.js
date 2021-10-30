9ea254d029365db531392ac7bb6a02fce8080116
Ellie-exe <ryiscool247@gmail.com>
2021-10-30 11:46:14 -0400

    Added an 8ball command

---
+++ src/commands/8ball.js
@@ -0,0 +1,53 @@
+module.exports = {
+    /** @param {import('discord.js/typings').CommandInteraction} command */
+    async execute(command) {
+        try {
+            const responses = [
+                'It is certain.',
+                'It is decidedly so.',
+                'Without a doubt.',
+                'Yes - definitely.',
+                'You may rely on it.',
+                'As I see it, yes.',
+                'Most likely.',
+                'Outlook good.',
+                'Yes.',
+                'Signs point to yes.',
+                'Reply hazy, try again.',
+                'Ask again later.',
+                'Better not tell you now.',
+                'Cannot predict now.',
+                'Concentrate and ask again.',
+                'Don\'t count on it.',
+                'My reply is no.',
+                'My sources say no.',
+                'Outlook not so good.',
+                'Very doubtful.'
+            ];
+
+            const question = command.options.getString('question');
+            const response = responses[Math.floor(Math.random() * responses.length)]
+
+            await command.reply(`**Question:** ${question}\n**Response:** ${response}`);
+
+        } catch (err) {
+            logger.error(err);
+        }
+    },
+
+    data: [
+        {
+            type: 'CHAT_INPUT',
+            name: '8ball',
+            description: 'Ask the Magic 8-ball a question',
+            options: [
+                {
+                    type: 'STRING',
+                    name: 'question',
+                    description: 'The question to ask',
+                    required: true
+                }
+            ]
+        }
+    ]
+};
