0a65dc27d7a12c1f64610b612dd3d7cbd52861a9
Ellie-exe <ryiscool247@gmail.com>
2021-12-03 20:43:00 -0500

    Added more events

--- src/events/index.js
+++ src/events/index.js
@@ -1,4 +1,9 @@
 module.exports = {
+    funkyMonkeyFriday: require('./funkyMonkeyFriday'),
+    guildMemberAdd: require('./guildMemberAdd'),
+    guildMemberUpdate: require('./guildMemberUpdate'),
     interactionCreate: require('./interactionCreate'),
+    messageCreate: require('./messageCreate'),
+    questionOfTheDay: require('./questionOfTheDay'),
     ready: require('./ready')
 }
