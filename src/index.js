fc41705a7c0112a349fb8a47f5c56c67a6346f80
Ellie-exe <ryiscool247@gmail.com>
2021-12-03 20:42:06 -0500

    Added more event triggers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const logger = require('@jakeyprime/logger');
+const schedule = require('node-schedule');
 const discord = require('discord.js');
 
-const client = new discord.Client({intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_PRESENCES']});
+const client = new discord.Client({intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_PRESENCES', 'GUILD_MESSAGES']});
 const sequelize = new Sequelize('senny', process.env.USERNAME, process.env.PASSWORD, {
     host: 'localhost',
     dialect: 'mariadb'
@@ -24,6 +25,18 @@ Object.values(commands).forEach(command => {
 const events = require('./events');
 Object.values(events).forEach(event => {
     client.on(event.name, (...args) => event.execute(...args));
+});
+
+schedule.scheduleJob('0 0 0 * * 5', async () => {
+    client.emit('funkyMonkeyFriday', 'Friday');
+});
+
+schedule.scheduleJob('0 0 0 * * 6', async () => {
+    client.emit('funkyMonkeyFriday', 'Saturday');
+});
+
+schedule.scheduleJob('0 0 12 * * *', async () => {
+    client.emit('questionOfTheDay');
 });
 
 client.login(process.env.TOKEN);
