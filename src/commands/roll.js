d4c0090b00d9f3a862ceb699cd43c611549f7142
Ellie-exe <ryiscool247@gmail.com>
2021-12-18 15:08:28 -0500

    Changed roll to support more NdN formats

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -2,40 +2,36 @@ module.exports = {
     /** @param {import('discord.js/typings').CommandInteraction} command */
     async execute(command) {
         try {
-            const dice = command.options.getString('sides');
+            const input = command.options.getString('sides');
 
-            if (dice.includes('d')) {
-                const die = dice.split('d');
-                const count = die[0];
-                const sides = die[1];
+            if (input.includes('d')) {
+                const rolls = input.split(/\s*\+\s*/g);
 
-                if (count < 0) return await command.reply('Cannot roll negative dice');
-                if (sides < 0) return await command.reply('Cannot roll dice with negative sides');
-                if (!count) return await command.reply('Cannot roll zero dice');
-                if (!sides) return await command.reply('Cannot roll dice with zero sides');
-                if (count % 1 !== 0) return await command.reply('Cannot roll partial dice');
-                if (sides % 1 !== 0) return await command.reply('Cannot roll dice with partial sides');
-
-                let rolls = [];
                 let total = 0;
+                let result = [];
 
-                for (let i = 0; i < count; i++) {
-                    const roll = Math.floor(Math.random() * sides + 1);
+                rolls.forEach(roll => {
+                    if (roll.includes('d')) {
+                        const dice = roll.split(/\s*d\s*/);
+                        const num = dice[0] || 1;
+                        const sides = dice[1];
 
-                    rolls.push(roll);
-                    total += roll;
-                }
+                        for (let i = 0; i < num; i++) {
+                            const value = Math.floor(Math.random() * sides) + 1;
 
-                await command.reply(`You rolled a **${rolls.join(', ')}** for a total of **${total}**`);
+                            result.push(value);
+                            total += value;
+                        }
+
+                    } else {
+                        total += roll;
+                    }
+                });
+
+                await command.reply(`You rolled a **${result.join(', ')}** for a total of **${total}**`);
 
             } else {
-                const roll = Math.floor(Math.random() * dice + 1);
-
-                if (dice < 0) return await command.reply('Cannot roll a die with negative sides');
-                if (!dice) return await command.reply('Cannot roll a die with zero sides');
-                if (dice % 1 !== 0) return await command.reply('Cannot roll a die with partial sides');
-
-                await command.reply(`You rolled a **${roll}**`);
+                await command.reply(`You rolled a **${Math.floor(Math.random() * input) + 1}**`);
             }
 
         } catch (err) {
