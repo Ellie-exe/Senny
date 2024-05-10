f6fc9805843b3b4ec263a9147ccbf17886482a35
Ryan Nelson <ryiscool247@gmail.com>
2024-05-10 16:00:37 -0400

    changed permissions

--- src/events/funkyMonkeyFriday.js
+++ src/events/funkyMonkeyFriday.js
@@ -10,20 +10,17 @@ module.exports = {
     async execute(day, client) {
         try {
             const guild = client.guilds.cache.get('376505103923806219');
-            const category = /** @type {import('discord.js').CategoryChannel} */ guild.channels.cache.get('772074946791866390');
             const channel = /** @type {import('discord.js').TextChannel} */ guild.channels.cache.get('738161372029911050');
             const role = guild.roles.cache.get('376505103923806219');
 
             switch (day) {
                 case 'Friday': {
-                    await category.permissionOverwrites.edit(role, { ViewChannel: true });
                     await channel.permissionOverwrites.edit(role, { ViewChannel: true });
                     break;
                 }
 
                 case 'Saturday': {
                     await channel.permissionOverwrites.edit(role, { ViewChannel: false });
-                    await category.permissionOverwrites.edit(role, { ViewChannel: false });
                     break;
                 }
             }
